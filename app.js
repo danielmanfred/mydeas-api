ce737ee42785251694e97f18a71362cf02b7dc14
danielmanfred <danielmanfred@gmail.com>
2018-05-19 23:47:16 -0300

    Criação do projeto e do servidor

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.send(200).json({
+        message: 'Está funcionando Perfeitamente bem!'
+    });
+});
